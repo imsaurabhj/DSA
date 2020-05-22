9e901a5f8b79b052358458991b2c11dde0810853
imsaurabhj <jhasaurabh12@gmail.com>
2020-05-22 22:41:45 +0530

    print all permutations of string

---
+++ string_permutations.cpp
@@ -0,0 +1,24 @@
+
+//This works only if string doesn't contain any duplicates
+
+#include<bits/stdc++.h>
+using namespace std;
+void permute(string str,int l,int r)
+{
+    if(l==r)
+    {
+        cout<<str<<endl;
+        return;
+    }
+    for(int i=l;i<=r;i++)
+    {
+        swap(str[l],str[i]);
+        permute(str,l+1,r);
+        swap(str[l],str[i]);
+    }
+}
+int main()
+{
+    string str="ABC";
+    permute(str,0,str.size()-1);
+}
